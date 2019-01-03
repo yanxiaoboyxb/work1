f6ca9cd28afca3d647cef86f5900000563e01040
zhangsan <zhangsan@itcast>
2019-01-03 19:18:59 +0800

    添加Hello类

---
+++ Git_Test1/src/main/java/cn/itcast/Hello.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+/***
+ * @Description:
+ * @ClassName: Hello
+ * @Author: Mr.Yan
+ * @Date: 2019/1/3 19:17
+ * */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
